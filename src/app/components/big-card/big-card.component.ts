d34bb0a24f83739ca2ddd4d010f72dba118e93b3
cristiano-araujo <cristiano112715@gmail.com>
2023-04-28 17:46:26 -0300

    ok

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,9 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+  @Input()
   photoCover:string="https://scontent.ffor30-1.fna.fbcdn.net/v/t39.30808-6/324895928_707943417619074_2132939391092169460_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeG6GT1tqxM0lVQYDzQu0HJ3oiUjkaFCGSiiJSORoUIZKIiksOFkki6ECyFtN8TjWoKfzqVchCucxweVhH1wPY4L&_nc_ohc=vblyZqLe9OsAX-hjgKz&_nc_ht=scontent.ffor30-1.fna&oh=00_AfABpaQfUPtJs3yU9a185gIVUjy_OBBWwG-rtIkj3D4xdA&oe=644E62D8"
+  @Input()
   cardTitle:string="Culto de Celebração aos Domingos!"
-  cardDescription:string="loren inoin"
+  @Input()
+  cardDescription:string="loren inoin ffor30-1.fna&oh=00_AfABpaQfUPtJs3yU9a185gIVUjy_OBBWwG-rtIkj3D4xdA&oe=644E62D8"
+ 
 
 
   constructor() {}
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover:string = "https://scontent.ffor30-1.fna.fbcdn.net/v/t39.30808-6/343035740_586528246874877_288692564593028498_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=xDC1LN353kIAX-ZK8nv&_nc_ht=scontent.ffor30-1.fna&oh=00_AfBAiZFPk56Jn-Y1jGoe___ri3Ny-tsFyEy7J__naZ6Jdw&oe=6450214C"
+  @Input()
+  cardTitle:string = "Visão e Missão"
+
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    
+  }
 
 }
